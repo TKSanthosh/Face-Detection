b8d03a287af525078704f48802357a025373f836
N.G. Santhosh Sudhaan <63006841+ngsanthosh@users.noreply.github.com>
2020-06-28 22:03:05 +0530

    Update image.py

--- image.py
+++ image.py
@@ -12,7 +12,9 @@ for (x, y, w, h) in faces_avail:
     cv2.rectangle(image, (x, y), (x + w, y + h), (200,20,60), 3)
     
 cv2.imshow('image', image)
-#Wait key takes the seconds (in ms) as parameter
+'''Wait key takes the seconds (in ms) as parameter
 #false indicates that image to be present forever 
-#true indicates that image to show only for 1 sec as true implys the value '1'
+#true indicates that image to show only for 1 sec as true implys the value '1''''
 cv2.waitKey(False)
+
+#The code is small right.!? That is because it is all about the Casecade Classifier(dataset) that is used within this, which is a haarcascade. :-)
