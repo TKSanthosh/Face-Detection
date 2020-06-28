9f9eef70f85eb6d6add8ac0cab2c83a7cd662c28
N.G. Santhosh Sudhaan <63006841+ngsanthosh@users.noreply.github.com>
2020-06-28 22:00:28 +0530

    Add files via upload

---
+++ image.py
@@ -0,0 +1,18 @@
+import cv2
+
+face_cascade_model = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+image = cv2.imread('msdhoni.jpg')
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+faces_avail = face_cascade_model.detectMultiScale(gray, 1.1, 5)
+
+for (x, y, w, h) in faces_avail:
+    cv2.rectangle(image, (x, y), (x + w, y + h), (200,20,60), 3)
+    
+cv2.imshow('image', image)
+#Wait key takes the seconds (in ms) as parameter
+#false indicates that image to be present forever 
+#true indicates that image to show only for 1 sec as true implys the value '1'
+cv2.waitKey(False)
